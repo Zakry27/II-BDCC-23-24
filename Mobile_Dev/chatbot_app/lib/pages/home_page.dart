6caaf09dc3f3a536aeff1fb64de932b635ac1395
Zakry27 <zak90078@gmail.com>
2024-07-20 18:19:28 +0100

    Flutter Chat Bot App Updated

---
+++ Mobile_Dev/chatbot_app/lib/pages/home_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // padding constants
+  final double horizontalPadding = 40;
+  final double verticalPadding = 25;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // app bar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+                vertical: verticalPadding,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // menu icon
+                  Image.asset(
+                    'lib/icons/menu.png',
+                    height: 45,
+                    color: Colors.grey[800],
+                  ),
+
+                  // account icon
+                  Icon(
+                    Icons.person,
+                    size: 45,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // welcome home
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome Home,",
+                    style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
+                  ),
+                  Text(
+                    'ZAKARIA CHARJAOUI',
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Divider(
+                thickness: 1,
+                color: Color.fromARGB(255, 204, 204, 204),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // smart devices grid
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Text(
+                "Latest Chat",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.grey.shade800,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Mobile_Dev/chatbot_flutter_app/android/app/src/main/kotlin/com/example/chatbot_flutter_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chatbot_flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
